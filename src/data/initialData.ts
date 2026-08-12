726eab51bb7106e0fa7ad8729c63c9fe603bb946
rickyanggara73059-cloud <rickyanggara73059@gmail.com>
2026-08-13 00:20:46 +0800

    Fix hero and logo assets

--- src/data/initialData.ts
+++ src/data/initialData.ts
@@ -12,7 +12,7 @@ export const initialCompanyProfileData: CompanyProfileData = {
   headOfficeAddress: "Jl. Raya Jember - Banyuwangi, Kec. Kabat, Kabupaten Banyuwangi, Jawa Timur 68461",
 
   heroImageUrl: "/images/altran_fleet_hero.jpg",
-  logoUrl: "/src/assets/images/altran_logo_1786547051420.jpg",
+  logoUrl: "/images/altran_logo.jpg",
 
   aboutHistory: "PT. ALTRAN GUNA USAHA memulai kiprah operasionalnya di industri jasa transportasi darat dan logistik sejak tahun 2017. Seiring dengan peningkatan kepercayaan pelanggan dan pertumbuhan kebutuhan angkutan industri nasional, perusahaan secara resmi berbadan hukum sebagai PT. ALTRAN GUNA USAHA pada tahun 2022.",
   
